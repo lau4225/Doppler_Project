6f1280471646a0a1f35a1df33c80015a45abfbb5
DioAn1730700 <anna7942009@hotmail.com>
2019-02-19 09:36:03 -0500

    projet no fxml

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,8 +2,13 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application{
@@ -17,15 +22,58 @@ public class Main extends Application{
     @Override
     public void start(Stage primaryStage) throws Exception{
 
-        Parent root = FXMLLoader.load(getClass().getResource("acceuil.fxml"));
-        Scene scene = new Scene(root, 700, 500);
+        BorderPane root = new BorderPane();
 
-        scene.getStylesheets().add("sample/style.css");
+        //SCENE1
+        Scene scene = new Scene(root);
 
         primaryStage.setTitle("Doppler Project");
-        //primaryStage.setMaximized(true);
+        primaryStage.setMaximized(true);
         primaryStage.setResizable(true);
         primaryStage.setScene(scene);
+
+        Label titre = new Label("DOPPLER AU PAYS DES BRUITS");
+        titre.setAlignment(Pos.TOP_CENTER);
+        Label noms = new Label("LAURIE BONNEL ET ANNABELLE DION");
+        noms.setAlignment(Pos.BOTTOM_CENTER);
+        Button demarrer = new Button("DÉMARRER");
+        demarrer.setAlignment(Pos.CENTER);
+        Button guideUti = new Button("GUIDE D'UTILISATION");
+        guideUti.setAlignment(Pos.CENTER);
+        root.setTop(titre);
+        root.setBottom(noms);
+        VBox centre  = new VBox(demarrer, guideUti);
+        centre.setAlignment(Pos.CENTER);
+        root.setCenter(centre);
+
+        //SCENE2
+        BorderPane root2 = new BorderPane();
+
+        Scene scene2  = new Scene(root2, 700, 500);
+
+        Button retour = new Button("Retour");
+        Label instructions = new Label("INSTRUCTIONS À ÉCRIRE ICI" +
+                "D\n" +
+                "D\n" +
+                "D\n" +
+                "D\n" +
+                "D\n" +
+                "D\n" +
+                "D\n" +
+                "D\n" +
+                "D\n");
+        VBox vBox = new VBox(instructions, retour);
+        vBox.setAlignment(Pos.CENTER);
+        root2.setCenter(vBox);
+
+        //ONACTION
+        guideUti.setOnAction(event -> {
+            primaryStage.setScene(scene2);
+        });
+
+        retour.setOnAction(event -> {
+            primaryStage.setScene(scene);
+        });
 
         primaryStage.show();
     }
